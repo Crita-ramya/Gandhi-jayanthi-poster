89d6d8539739affa9b56822e19deee6aaced61aa
Crita-ramya <137986118+Crita-ramya@users.noreply.github.com>
2023-10-02 12:26:29 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1 className="header">Happy Gandhi Jyanthi</h1>
+      <p className="para">
+        Remembering the Mahatma: Celebrating the Legacy of Gandhi on His Birth
+        Anniversary <span>🙏</span>
+      </p>
+      <div>
+        <img
+          src="https://res.cloudinary.com/ddsus8zy7/image/upload/v1696227970/18148604_oyaqf5.jpg"
+          alt=""
+          className="image"
+        />
+      </div>
+    </div>
+  );
+}
